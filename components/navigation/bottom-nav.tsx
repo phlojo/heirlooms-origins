fe756212fb0c5a561e55829038f2658d83d4aa0b
Phlojo <floringale@gmail.com>
2025-11-27 13:44:02 -0500

    overscroll fix attempt again

--- components/navigation/bottom-nav.tsx
+++ components/navigation/bottom-nav.tsx
@@ -68,7 +68,7 @@ export default function BottomNav() {
     <nav
       className={cn(
         "fixed left-0 right-0 bottom-0 z-50",
-        "border-t bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/80",
+        "border-t bg-background",
         "lg:hidden",
       )}
       style={{
